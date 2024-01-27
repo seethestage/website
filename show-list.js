65e46236129aea9ef9bba906c36aa43377d25c20
seethestage <154288847+seethestage@users.noreply.github.com>
2024-01-27 17:44:40 -0500

    Update show-list.js

--- show-list.js
+++ show-list.js
@@ -267,3 +267,245 @@ document.addEventListener('click', function(event) {
         closestDayBtnGroup.classList.add('is-active');
     }
 });
+
+
+// JavaScript code to scroll 'location-dropdown' to the top
+// when a user types in the 'location-search' input
+
+// First, get the elements by their IDs
+var locationSearch = document.getElementById('location-search');
+var locationDropdown = document.getElementById('location-dropdown');
+
+// Add an event listener for the 'keyup' event on the location search input
+locationSearch.addEventListener('keyup', function() {
+    // Scroll the location dropdown to the top
+    locationDropdown.scrollTop = 0;
+});
+
+
+
+
+
+// BOOKMARKS SCRIPT
+//Initial load of bookmarked shows
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Function to process shows
+    const processShows = () => {
+        const bookmarkedShowsText = document.getElementById('bookmarked-shows').textContent;
+        const showValues = bookmarkedShowsText.match(/\[(.*?)\]/g).map(val => val.slice(1, -1));
+
+        setTimeout(() => {
+            const showIdTextDivs = document.querySelectorAll('.show-bookmark-id');
+
+            showIdTextDivs.forEach(div => {
+                if (showValues.includes(div.textContent.trim())) {
+                    let sibling = div.nextElementSibling;
+                    while (sibling) {
+                        if (sibling.classList.contains('bookmark-selected')) {
+                            sibling.style.display = 'block';
+                            console.log("Updated display for:", sibling);
+                            break;
+                        }
+                        sibling = sibling.nextElementSibling;
+                    }
+                }
+            });
+        }, 100);
+    };
+
+    // Run once at the start
+    processShows();
+
+    // Event listener for clicking the 'load-more-shows-btn'
+    document.getElementById('load-more-shows-btn').addEventListener('click', processShows);
+// Event listener for clicking on any div with class 'dropdown-link'
+document.querySelectorAll('.dropdown-link').forEach(function(element) {
+    element.addEventListener('click', processShows);
+});
+
+    // MutationObserver to watch for changes in elements with class 'tag'
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === 'childList' && mutation.addedNodes.length > 0 || mutation.removedNodes.length > 0) {
+                // Check if any of the added or removed nodes have class 'tag'
+                const nodes = [...mutation.addedNodes, ...mutation.removedNodes];
+                const hasTagClass = nodes.some(node => node.classList && node.classList.contains('tag'));
+
+                if (hasTagClass) {
+                    processShows();
+                }
+            }
+        });
+    });
+
+    // Observe the document body for addition or removal of 'tag' elements
+    observer.observe(document.body, { childList: true, subtree: true });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('click', function(event) {
+        if (event.target.classList.contains('bookmark-unselected')) {
+            // Handling for bookmark-unselected
+            handleBookmarkUnselected(event.target);
+        } else if (event.target.classList.contains('bookmark-selected')) {
+            // Handling for bookmark-selected
+            handleBookmarkSelected(event.target);
+        }
+    });
+});
+
+function handleBookmarkUnselected(clickedDiv) {
+    // Find and display the sibling with class 'bookmark-selected'
+    const bookmarkSelected = clickedDiv.parentNode.querySelector('.bookmark-selected');
+    if (bookmarkSelected) {
+        bookmarkSelected.style.display = 'block';
+    }
+
+    // Find the sibling with class 'show-bookmark-id' and get its text
+    const showIdTxt = clickedDiv.parentNode.querySelector('.show-bookmark-id');
+    if (showIdTxt) {
+        const showId = `[${showIdTxt.textContent}]`;
+        const bookmarkedShowsField = document.getElementById('bookmarked-shows');
+        bookmarkedShowsField.value += showId;
+    }
+
+    // Simulate click on the 'update-bookmarks-btn'
+    const updateBtn = document.getElementById('update-bookmarks-btn');
+    updateBtn.click();
+}
+
+function handleBookmarkSelected(clickedDiv) {
+    // Hide the clicked div
+    clickedDiv.style.display = 'none';
+
+    // Find the sibling with class 'show-bookmark-id' and get its text
+    const showIdTxt = clickedDiv.parentNode.querySelector('.show-bookmark-id');
+    if (showIdTxt) {
+        const showId = `[${showIdTxt.textContent}]`;
+        const bookmarkedShowsField = document.getElementById('bookmarked-shows');
+
+        // Check if the showId is in the bookmarked shows and remove it
+        if (bookmarkedShowsField.value.includes(showId)) {
+            bookmarkedShowsField.value = bookmarkedShowsField.value.replace(showId, '');
+        }
+    }
+
+    // Simulate click on the 'update-bookmarks-btn'
+    const updateBtn = document.getElementById('update-bookmarks-btn');
+    updateBtn.click();
+}
+
+
+
+
+
+
+// STARRED SCRIPT
+//Initial load of starred shows
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Function to process shows
+    const processShows = () => {
+        const starredShowsText = document.getElementById('starred-shows').textContent;
+        const showValues = starredShowsText.match(/\[(.*?)\]/g).map(val => val.slice(1, -1));
+
+        setTimeout(() => {
+            const showIdTextDivs = document.querySelectorAll('.show-starred-id');
+
+            showIdTextDivs.forEach(div => {
+                if (showValues.includes(div.textContent.trim())) {
+                    let sibling = div.nextElementSibling;
+                    while (sibling) {
+                        if (sibling.classList.contains('starred-selected')) {
+                            sibling.style.display = 'block';
+                            console.log("Updated display for:", sibling);
+                            break;
+                        }
+                        sibling = sibling.nextElementSibling;
+                    }
+                }
+            });
+        }, 100);
+    };
+
+    // Run once at the start
+    processShows();
+
+    // Event listener for clicking the 'load-more-shows-btn'
+    document.getElementById('load-more-shows-btn').addEventListener('click', processShows);
+
+// Event listener for clicking on any div with class 'dropdown-link'
+document.querySelectorAll('.dropdown-link').forEach(function(element) {
+    element.addEventListener('click', processShows);
+});
+
+    // MutationObserver to watch for changes in elements with class 'tag'
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === 'childList' && mutation.addedNodes.length > 0 || mutation.removedNodes.length > 0) {
+                // Check if any of the added or removed nodes have class 'tag'
+                const nodes = [...mutation.addedNodes, ...mutation.removedNodes];
+                const hasTagClass = nodes.some(node => node.classList && node.classList.contains('tag'));
+
+                if (hasTagClass) {
+                    processShows();
+                }
+            }
+        });
+    });
+
+    // Observe the document body for addition or removal of 'tag' elements
+    observer.observe(document.body, { childList: true, subtree: true });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('click', function(event) {
+        if (event.target.classList.contains('starred-unselected')) {
+            // Handling for starred-unselected
+            handleStarredUnselected(event.target);
+        } else if (event.target.classList.contains('starred-selected')) {
+            // Handling for starred-selected
+            handleStarredSelected(event.target);
+        }
+    });
+});
+
+function handleStarredUnselected(clickedDiv) {
+    // Find and display the sibling with class 'starred-selected'
+    const starredSelected = clickedDiv.parentNode.querySelector('.starred-selected');
+    if (starredSelected) {
+        starredSelected.style.display = 'block';
+    }
+
+    // Find the sibling with class 'show-starred-id' and get its text
+    const showIdTxt = clickedDiv.parentNode.querySelector('.show-starred-id');
+    if (showIdTxt) {
+        const showId = `[${showIdTxt.textContent}]`;
+        const starredShowsField = document.getElementById('starred-shows');
+        starredShowsField.value += showId;
+    }
+
+    // Simulate click on the 'update-starred-btn'
+    const updateBtn = document.getElementById('update-starred-btn');
+    updateBtn.click();
+}
+
+function handleStarredSelected(clickedDiv) {
+    // Hide the clicked div
+    clickedDiv.style.display = 'none';
+
+    // Find the sibling with class 'show-starred-id' and get its text
+    const showIdTxt = clickedDiv.parentNode.querySelector('.show-starred-id');
+    if (showIdTxt) {
+        const showId = `[${showIdTxt.textContent}]`;
+        const starredShowsField = document.getElementById('starred-shows');
+
+        // Check if the showId is in the starred shows and remove it
+        if (starredShowsField.value.includes(showId)) {
+            starredShowsField.value = starredShowsField.value.replace(showId, '');
+        }
+    }
+
+    // Simulate click on the 'update-starred-btn'
+    const updateBtn = document.getElementById('update-starred-btn');
+    updateBtn.click();
+}
+
+
